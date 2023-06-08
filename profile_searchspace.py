abf1c686fded846a67bcc932977b0bbd8e638f1d
fjwillemsen <fjwillemsen@icloud.com>
2023-06-08 17:49:46 +0200

    Script to profile Searchspace initialization times

---
+++ profile_searchspace.py
@@ -0,0 +1,34 @@
+"""A script to profile Searchspace initialization times."""
+
+from test_searchspace import generate_searchspace, run_searchspace_initialization
+import cProfile, pstats, io
+import yappi
+
+tune_params, restrictions = generate_searchspace()
+
+
+def run():
+    run_searchspace_initialization(tune_params=tune_params, restrictions=restrictions)
+
+
+def profile_cprof():
+    pr = cProfile.Profile()
+    pr.enable()
+    run()
+    pr.disable()
+    pr.dump_stats("profile.prof")
+
+
+def profile_yappi():
+    """Entry point for execution."""
+    yappi.set_clock_type("cpu")  # Use set_clock_type("wall") for wall time
+    yappi.start()
+    run()
+    yappi.stop()
+    yappi.get_func_stats().print_all()
+    yappi.get_thread_stats().print_all()
+
+
+if __name__ == "__main__":
+    profile_cprof()
+    # profile_yappi()
