32040d5ead8af5737f9891da689dca2263bbe971
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2024-01-18 08:34:49 +0100

    Renamed cachefiles for parsing convenience

--- kernels/noisetest_convolution.py
+++ kernels/noisetest_convolution.py
@@ -3,18 +3,22 @@
 """Tuning script to tune the convolution kernel. Based on https://github.com/benvanwerkhoven/energy_experiments/blob/master/algorithm/convolution.py."""
 
 
+import json
 import os
 import time
-import json
 
-import numpy
 import kernel_tuner
-
-from common import get_metrics, get_device_name, get_nvcc_cuda_version_string, check_pycuda_version_matches_cuda
+import numpy
+from common import (
+    check_pycuda_version_matches_cuda,
+    get_device_name,
+    get_metrics,
+    get_nvcc_cuda_version_string,
+)
 
 
 def ops(w, h, fw, fh):
-    return (w * h * fw * fh * 2)/1e9
+    return (w * h * fw * fh * 2) / 1e9
 
 
 def tune(inputs, device=0):
@@ -22,27 +26,42 @@ def tune(inputs, device=0):
     image_width, image_height, filter_width, filter_height = inputs
 
     # kernel string
-    with open(os.path.dirname(os.path.realpath(__file__)) + '/convolution/convolution.cu', 'r') as f:
+    with open(
+        os.path.dirname(os.path.realpath(__file__)) + "/convolution/convolution.cu", "r"
+    ) as f:
         kernel_string = f.read()
 
     # tunable parameters
-    tune_params = { "block_size_x": [64], "block_size_y": [2], "tile_size_x": [1], "tile_size_y": [4], "read_only": [1], "use_padding": [0] }
+    tune_params = {
+        "block_size_x": [64],
+        "block_size_y": [2],
+        "tile_size_x": [1],
+        "tile_size_y": [4],
+        "read_only": [1],
+        "use_padding": [0],
+    }
     tune_params["REPEAT"] = [i for i in range(1000)]
 
     # restrictions: limit the search to only use padding when its effective
-    restrict = ["(use_padding==0 or (block_size_x % 32 != 0))", "((block_size_x*tile_size_x+4)*(block_size_y*tile_size_y+4) < 12*1024)"]
-    restrict.append("(((block_size_x*tile_size_x+%d)*(block_size_y*tile_size_y+%d)) < 12*1024)" % (filter_width-1, filter_height-1))
+    restrict = [
+        "(use_padding==0 or (block_size_x % 32 != 0))",
+        "((block_size_x*tile_size_x+4)*(block_size_y*tile_size_y+4) < 12*1024)",
+    ]
+    restrict.append(
+        "(((block_size_x*tile_size_x+%d)*(block_size_y*tile_size_y+%d)) < 12*1024)"
+        % (filter_width - 1, filter_height - 1)
+    )
 
     # additional arguments
     problem_size = (image_width, image_height)
     size = numpy.prod(problem_size)
     largest_fh = filter_height
     largest_fw = filter_width
-    input_size = ((problem_size[0]+largest_fw-1) * (problem_size[1]+largest_fh-1))
+    input_size = (problem_size[0] + largest_fw - 1) * (problem_size[1] + largest_fh - 1)
     output_image = numpy.zeros(size).astype(numpy.float32)
     input_image = numpy.random.randn(input_size).astype(numpy.float32)
     filter_weights = numpy.random.randn(largest_fh * largest_fw).astype(numpy.float32)
-    cmem_args = {'d_filter': filter_weights}
+    cmem_args = {"d_filter": filter_weights}
     args = [output_image, input_image, filter_weights]
     grid_div_x = ["block_size_x", "tile_size_x"]
     grid_div_y = ["block_size_y", "tile_size_y"]
@@ -54,28 +73,43 @@ def tune(inputs, device=0):
     backends = ["CUPY"]
     cuda_version = get_nvcc_cuda_version_string()
     for backend in backends:
-        filename = f"outputdata/convolution_{device_name}_noisetest_{backend}_{cuda_version}"
+        filename = f"outputdata/convolution_{device_name}_noisetest_backend-{backend}_CUDA-{cuda_version}"
         print(f"{filename=}")
 
-        #start tuning
+        # start tuning
         start = time.time()
-        results, env = kernel_tuner.tune_kernel("convolution_kernel", kernel_string,
-                        problem_size, args, tune_params,
-                        grid_div_y=grid_div_y, grid_div_x=grid_div_x, cmem_args=cmem_args,
-                        device=device, platform=0, lang=backend, verbose=True, metrics=metrics, 
-                        restrictions=restrict, iterations=30, cache=filename + "_cache.json")
+        results, env = kernel_tuner.tune_kernel(
+            "convolution_kernel",
+            kernel_string,
+            problem_size,
+            args,
+            tune_params,
+            grid_div_y=grid_div_y,
+            grid_div_x=grid_div_x,
+            cmem_args=cmem_args,
+            device=device,
+            platform=0,
+            lang=backend,
+            verbose=True,
+            metrics=metrics,
+            restrictions=restrict,
+            iterations=30,
+            cache=filename + "_cache.json",
+        )
         end = time.time()
-        env["execution_time"] = end-start
+        env["execution_time"] = end - start
 
         # write outputs
-        with open(filename + "_output.json", 'w') as fh:
+        with open(filename + "_output.json", "w") as fh:
             json.dump(results, fh)
-        with open(filename + "_env.json", 'w') as fh:
+        with open(filename + "_env.json", "w") as fh:
             json.dump(env, fh)
 
 
 if __name__ == "__main__":
     w = h = 4096
     fw = fh = 17
-    assert check_pycuda_version_matches_cuda(), f"PyCUDA was compiled against a different CUDA version than the current CUDA version"
-    tune([w,h,fw,fh], device=0)
+    assert (
+        check_pycuda_version_matches_cuda()
+    ), "PyCUDA was compiled against a different CUDA version than the current CUDA version"
+    tune([w, h, fw, fh], device=0)
--- kernels/noisetest_gemm.py
+++ kernels/noisetest_gemm.py
@@ -3,18 +3,17 @@
 """Tuning script to test the performance and noise of different backends. Uses the best parameter configuration on the A4000 as a basis. Based on https://github.com/benvanwerkhoven/energy_experiments/blob/master/algorithm/gemm1000.py."""
 
 
-import time
-import os
 import json
+import os
+import time
 
-import numpy as np
 import kernel_tuner
-
-from common import get_metrics, get_device_name
+import numpy as np
+from common import get_device_name, get_metrics
 
 
 def ops(m, n, k):
-    return (m * n * k * 2 + 2 * m * k)/1e9
+    return (m * n * k * 2 + 2 * m * k) / 1e9
 
 
 def tune(inputs, device=0):
@@ -23,32 +22,52 @@ def tune(inputs, device=0):
 
     # kernel string
     kernel_string = ""
-    files = ["common.opencl", "xgemm_part1.opencl", "xgemm_part2.opencl", "xgemm_part3.opencl"]
+    files = [
+        "common.opencl",
+        "xgemm_part1.opencl",
+        "xgemm_part2.opencl",
+        "xgemm_part3.opencl",
+    ]
     for f in files:
         with open(path + f, "r") as fp:
             kernel_string += fp.read()
 
-    #n = np.int32(32)
-    #m = np.int32(16)
-    #k = np.int32(32)
+    # n = np.int32(32)
+    # m = np.int32(16)
+    # k = np.int32(32)
     m, n, k = [np.int32(i) for i in inputs]
 
-    #// Matrices are accessed as follows:
-    #// A: [k*M + m], with 'k' ranging from 0:K and 'm' from 0:M (m,k,m)
-    #// B: [k*N + n], with 'k' ranging from 0:K and 'n' from 0:N (n,k,n)
-    #// C: [n*M + m], with 'n' ranging from 0:N and 'm' from 0:M (m,n,m)
+    # // Matrices are accessed as follows:
+    # // A: [k*M + m], with 'k' ranging from 0:K and 'm' from 0:M (m,k,m)
+    # // B: [k*N + n], with 'k' ranging from 0:K and 'n' from 0:N (n,k,n)
+    # // C: [n*M + m], with 'n' ranging from 0:N and 'm' from 0:M (m,n,m)
 
     # input / output array intialization
-    A = np.array(np.random.randn(m, k), order='F').astype(np.float32)
-    B = np.array(np.random.randn(k, n), order='F').astype(np.float32)
-    #C = np.array(np.random.randn(m, n), order='F').astype(np.float32)
-    C = np.zeros((m, n), order='F').astype(np.float32)
+    A = np.array(np.random.randn(m, k), order="F").astype(np.float32)
+    B = np.array(np.random.randn(k, n), order="F").astype(np.float32)
+    # C = np.array(np.random.randn(m, n), order='F').astype(np.float32)
+    C = np.zeros((m, n), order="F").astype(np.float32)
     alpha, beta = np.random.randn(2).astype(np.float32)
     alpha, beta = np.array([1.0, 1.0]).astype(np.float32)
 
     # tunable parameters
-    tune_params = {"MWG": [128], "NWG": [128], "MDIMC": [16], "NDIMC": [8], "MDIMA": [16], "NDIMB": [32],
-                   "VWM": [8], "VWN": [4], "SA": [1], "SB": [1], "KWG": [32], "KWI": [2], "STRM": [0], "STRN": [0], "PRECISION": [32]}
+    tune_params = {
+        "MWG": [128],
+        "NWG": [128],
+        "MDIMC": [16],
+        "NDIMC": [8],
+        "MDIMA": [16],
+        "NDIMB": [32],
+        "VWM": [8],
+        "VWN": [4],
+        "SA": [1],
+        "SB": [1],
+        "KWG": [32],
+        "KWI": [2],
+        "STRM": [0],
+        "STRN": [0],
+        "PRECISION": [32],
+    }
     tune_params["REPEAT"] = [i for i in range(1000)]
 
     # restrictions
@@ -74,26 +93,41 @@ def tune(inputs, device=0):
     # backend selection
     backends = ["OpenCL"]
     for backend in backends:
-        filename = f"outputdata/GEMM_{device_name}_noisetest_{backend}"
+        filename = f"outputdata/GEMM_{device_name}_noisetest_backend-{backend}"
         print(f"{filename=}")
 
         # start tuning
         start = time.time()
-        results, env = kernel_tuner.tune_kernel("Xgemm", kernel_string, problem_size, args, tune_params, block_size_names=block_size_names,
-                                lang=backend, restrictions=restrict, verbose=True, compiler_options=["-I"+path],
-                                grid_div_x=grid_div_x, grid_div_y=grid_div_y,
-                                device=device, platform=0, iterations=30, metrics=metrics,
-                                cache=filename + "_cache.json", simulation_mode=False)
+        results, env = kernel_tuner.tune_kernel(
+            "Xgemm",
+            kernel_string,
+            problem_size,
+            args,
+            tune_params,
+            block_size_names=block_size_names,
+            lang=backend,
+            restrictions=restrict,
+            verbose=True,
+            compiler_options=["-I" + path],
+            grid_div_x=grid_div_x,
+            grid_div_y=grid_div_y,
+            device=device,
+            platform=0,
+            iterations=30,
+            metrics=metrics,
+            cache=filename + "_cache.json",
+            simulation_mode=False,
+        )
         end = time.time()
-        env['execution_time'] = end-start
+        env["execution_time"] = end - start
 
         # write outputs
-        with open(filename + "_output.json", 'w') as fh:
+        with open(filename + "_output.json", "w") as fh:
             json.dump(results, fh)
-        with open(filename + "_env.json", 'w') as fh:
+        with open(filename + "_env.json", "w") as fh:
             json.dump(env, fh)
 
 
 if __name__ == "__main__":
     m = n = k = 4096
-    tune([m,n,k], device=0)
+    tune([m, n, k], device=0)
