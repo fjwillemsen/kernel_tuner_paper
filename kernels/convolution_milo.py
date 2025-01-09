d8d0a0a25073a9478b90de93557eab5d01c3d4b5
fjwillemsen <fjwillemsen@icloud.com>
2025-01-09 09:59:09 +0100

    Added Milo convolution kernel and convolution output

---
+++ kernels/convolution_milo.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+
+"""Tuning script to tune the convolution kernel. Based on https://github.com/benvanwerkhoven/energy_experiments/blob/master/algorithm/convolution.py."""
+
+
+import argparse
+import json
+import os
+import time
+import itertools
+import subprocess
+import sys
+
+
+
+def ops(w, h, fw, fh):
+    return (w * h * fw * fh * 2) / 1e9
+
+
+def tune(inputs, device=0, id=None, ensemble=None):
+    # do imports
+    import kernel_tuner
+    import numpy
+    from common import get_device_name, get_metrics, get_fallback
+    from kernel_tuner.observers.nvml import NVMLObserver
+
+    # get inputs
+    device_name = get_device_name(device)
+    image_width, image_height, filter_width, filter_height = inputs
+
+    # kernel string
+    with open(
+        os.path.dirname(os.path.realpath(__file__)) + "/convolution/convolution.cu", "r"
+    ) as f:
+        kernel_string = f.read()
+
+    # tunable parameters
+    tune_params = dict()
+    tune_params["block_size_x"] = [16 * i for i in range(1, 17)]
+    tune_params["block_size_y"] = [2**i for i in range(5)]
+    tune_params["tile_size_x"] = [i for i in range(1, 5)]
+    tune_params["tile_size_y"] = [i for i in range(1, 5)]
+    tune_params["read_only"] = [0, 1]  # toggle using the read-only cache
+    tune_params["use_padding"] = [
+        0,
+        1,
+    ]  # toggle the insertion of padding in shared memory
+
+    # restrictions: limit the search to only use padding when its effective
+    restrict = [
+        "(use_padding==0 or (block_size_x % 32 != 0))",
+        "((block_size_x*tile_size_x+4)*(block_size_y*tile_size_y+4) < 12*1024)",
+    ]
+    restrict.append(
+        "(((block_size_x*tile_size_x+%d)*(block_size_y*tile_size_y+%d)) < 12*1024)"
+        % (filter_width - 1, filter_height - 1)
+    )
+
+    # additional arguments
+    strategy = "basinhopping"
+    strategy_options = {}
+    # strategy_options["ensemble"] = ensemble
+    problem_size = (image_width, image_height)
+    size = numpy.prod(problem_size)
+    largest_fh = filter_height
+    largest_fw = filter_width
+    input_size = (problem_size[0] + largest_fw - 1) * (problem_size[1] + largest_fh - 1)
+    output_image = numpy.zeros(size).astype(numpy.float32)
+    input_image = numpy.random.randn(input_size).astype(numpy.float32)
+    filter_weights = numpy.random.randn(largest_fh * largest_fw).astype(numpy.float32)
+    cmem_args = {"d_filter": filter_weights}
+    input_args = [output_image, input_image, filter_weights]
+    grid_div_x = ["block_size_x", "tile_size_x"]
+    grid_div_y = ["block_size_y", "tile_size_y"]
+    total_flops = ops(*inputs)
+    metrics = get_metrics(total_flops)
+    filename = f"outputdata/convolution/variable_clock_size-4096/original_tuning/convolution_{device_name}"
+    print(f"{filename=}")
+
+    # start tuning
+    start = time.time()
+    results, env = kernel_tuner.tune_kernel(
+        "convolution_kernel",
+        kernel_string,
+        problem_size,
+        input_args,
+        tune_params,
+        simulation_mode=True,
+        #parallel_mode=True,
+        grid_div_y=grid_div_y,
+        grid_div_x=grid_div_x,
+        cmem_args=cmem_args,
+        device=device,
+        platform=0,
+        verbose=False,
+        metrics=metrics,
+        restrictions=restrict,
+        iterations=32,
+        strategy=strategy,
+        strategy_options=strategy_options,
+        cache=filename + "_cache.json",
+        log=False,
+    )
+    end = time.time()
+    env["execution_time"] = end - start
+
+    if strategy == "ensemble": id_str = f"_{id}"
+    else: id_str = ""
+    # # write outputs
+    # with open(filename + f"_{strategy}{id_str}_output.json", "w") as fh:
+    #     json.dump(results, fh)
+    # with open(filename + f"_{strategy}{id_str}_env.json", "w") as fh:
+    #     json.dump(env, fh)
+    return results, env
+
+def copy_file(source, destination):
+    command = ['cp', source, destination]
+    
+    try:
+        subprocess.run(command, check=True)
+        print("File copied successfully.")
+    except subprocess.CalledProcessError:
+        print("Error copying file.")
+
+
+strategy_list = ["random_sample", "minimize", "basinhopping", "diff_evo", "genetic_algorithm", 
+                 "greedy_mls", "ordered_greedy_mls", "greedy_ils", "dual_annealing", "mls", "pso", 
+                 "simulated_annealing", "firefly_algorithm", "bayes_opt"]
+
+if __name__ == "__main__":
+    
+    strategy_combinations = []
+    for r in range(2, 5):  # From 2 to 4 strategies
+        strategy_combinations.extend(itertools.combinations(strategy_list, r))
+
+    strategy_combinations_with_id = {f"ID_{i}": combination for i, combination in enumerate(strategy_combinations, start=1)}
+
+    # tune
+    w = h = 4096
+    fw = fh = 15
+    benchmark_results = []
+
+    # for id, combination in strategy_combinations_with_id.items():
+    #     print(f"ID: {id}, ensemble: {combination}", sys.stderr)
+    #     results, env = tune(inputs=[w, h, fw, fh], id=id, ensemble=combination)
+    #     benchmark_results.append({
+    #         "ID": id,
+    #         "strategies": combination,
+    #         "results": results,
+    #         "env": env
+    #     })
+
+    # for item in benchmark_results:
+    #     item["strategies"] = list(item["strategies"])
+
+    # with open('ensembles_benchmark_results.json', 'w') as json_file:
+    #     json.dump(benchmark_results, json_file, indent=4)
+
+    results, env = tune(inputs=[w, h, fw, fh])
