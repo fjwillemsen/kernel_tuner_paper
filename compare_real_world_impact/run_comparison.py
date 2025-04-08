5189bb3278db0f5e6917784c420bd895bd6978f0
fjwillemsen <fjwillemsen@icloud.com>
2025-04-08 10:41:59 +0200

    New results for comparing real-world impact of searchspace constructio

--- compare_real_world_impact/run_comparison.py
+++ compare_real_world_impact/run_comparison.py
@@ -6,7 +6,7 @@ from kernels.hotspot.hotspot import tune as tune_hotspot
 
 kernels = ["hotspot"]           # names of the kernel and folder in the kernels folder (must be the same)
 platforms = [("CUDA", "A4000")] # tuple of language and device, for language choose from CUDA, HIP and OpenCL
-iterations = 1                  # number of times to repeat each tuning run
+iterations = 10                 # number of times to repeat each tuning run
 searchspace_constructors = [    # the searchspace construction frameworks to use
     "pythonconstraint",
     "pyatf",
@@ -14,14 +14,14 @@ searchspace_constructors = [    # the searchspace construction frameworks to use
 ]
 
 # execute the tuning for each combination
-for searchspace_constructor in searchspace_constructors:
-    for kernel in kernels:
-        if len(kernels) > 1:
-            print(f"running kernel {kernel}:")
-        for language, device in platforms:
-            if len(platforms) > 1:
-                print(f"  on {device=} with {language=}:")
-            for iteration in range(iterations):
+for iteration in range(iterations):
+    for searchspace_constructor in searchspace_constructors:
+        for kernel in kernels:
+            if len(kernels) > 1:
+                print(f"running kernel {kernel}:")
+            for language, device in platforms:
+                if len(platforms) > 1:
+                    print(f"  on {device=} with {language=}:")
                 # create the cache path and skip this combination if it already exists
                 cachefile_path = Path(f"results/hotspot/{device.upper()}_f={searchspace_constructor}_i={iteration}.json")
                 if cachefile_path.exists():
